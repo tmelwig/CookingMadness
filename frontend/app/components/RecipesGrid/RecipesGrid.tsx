2f50260ded1e0068344b213a48f1544ca28f6408
tmelwig <thomas.melwig@student-cs.fr>
2025-03-11 10:04:10 +0100

    [RecipesGrid] Create a RecipesGrid component

---
+++ frontend/app/components/RecipesGrid/RecipesGrid.tsx
@@ -0,0 +1,28 @@
+"use client";
+import React from "react";
+import { useListRecipes } from "@/app/hooks/RecipesHooks";
+import { useRecipe } from '@/app/contexts/RecipeContext';
+import Recipe from "@/app/types/recipe";
+import RecipeCell from "./RecipeCell";
+
+const RecipesGrid: React.FC = () => {
+  const { recipes, loading, error } = useListRecipes();
+  const { setSelectedRecipe } = useRecipe();
+
+  function handleRecipeClick(recipe: Recipe) {
+    setSelectedRecipe(recipe);
+  }
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
+
+  return (
+    <div className="grid gap-4 md:gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 my-2 md:my-6">
+      {recipes.map((recipe) => (
+        <RecipeCell key={recipe.id} recipe={recipe} onClick={handleRecipeClick} />
+      ))}
+    </div>
+  );
+};
+
+export default RecipesGrid;
--- frontend/app/recettes/page.tsx
+++ frontend/app/recettes/page.tsx
@@ -1,13 +1,18 @@
-// "use client";
-import React, {JSX} from "react";
-import Text from "@/app/components/Text";
+"use client";
+import React, { JSX } from "react";
+import RecipesGrid from "@/app/components/RecipesGrid/RecipesGrid";
 import NavBar from "@/app/components/NavBar";
+import Text from "@/app/components/Text";
 
-export default function RecipesPage(): JSX.Element {
-    return (
-      <div>
-        <NavBar/>
+export default function Home(): JSX.Element {
+  return (
+    <div>
+      <NavBar />
+      <div className="p-5 bg-gray-100">
         <Text variant="title-h1">Recettes</Text>
+        <Text variant="description">Voici une liste de super recettes à essayer rapidement !</Text>
+        <RecipesGrid />
       </div>
-    );
-  };
+    </div>
+  );
+}
